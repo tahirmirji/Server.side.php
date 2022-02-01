abb8c729402fd18406c210a36c4c3dcffe4070a1
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-02-01 12:45:49 +0530

    conenction with database creation

--- php_mysql_demo.php
+++ php_mysql_demo.php
@@ -13,6 +13,19 @@ if($connection ->connect_error)
     die("Hey guys we have a problem, Connection Error".$connection->connect_error);
 }
 
-    echo "MYSQL Database Connected Successfully";
+    echo "MYSQL Database Connected Successfully <br/>";
+
+if($connection->query("CREATE DATABASE wiz"))
+{
+    echo "Database created successfully.<br/>";
+}
+else
+{
+    echo "Database creation failed.<br/>";
+    exit();
+}
+
+$connection->close();
+
 
 ?>
