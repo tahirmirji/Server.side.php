a67be47308d1e657acb1387bb4d2fb61cd0d5b6c
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-02-01 13:17:22 +0530

    Assignment

--- php_mysql_demo.php
+++ php_mysql_demo.php
@@ -17,6 +17,17 @@ if($connection ->connect_error)
 
 echo "MYSQL Database Connected Successfully <br/>";
 
+$query1 ="create table  employee(empid INT NOT NULL AUTO_INCREMENT,empname varchar(40) NOT NULL, empsal double(5,1) NOT NULL, empemail   varchar(50) NOT NULL, PRIMARY KEY(empid))";
+
+$query2 = "create table ";
+
+$query3 = "create table ";
+
+$query4 = "create table ";
+
+$query5 = "create table ";
+
+
 
 if($connection->query("create table  employee(empid INT NOT NULL AUTO_INCREMENT,empname varchar(40) NOT NULL, empsal double(5,1) NOT NULL, empemail   varchar(50) NOT NULL, PRIMARY KEY(empid))"))
 {
